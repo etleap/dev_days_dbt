515a6f4c392aad5f9802da228efb727ade7c7736
Shannon Sangha <sksangha@Shannons-MBP.hsd1.ca.comcast.net>
2024-11-22 08:57:24 -0800

    spend_per_user now references stg_users as well as costs

--- models/metrics/spend_per_user.sql
+++ models/metrics/spend_per_user.sql
@@ -6,13 +6,17 @@
 WITH user_spends AS (
     SELECT
         costs.user_id,
+        users.name AS user_name, -- Adding the user name column
         SUM(costs.total_cost) AS total_spend
     FROM {{ ref('costs') }} AS costs
-    GROUP BY costs.user_id
+    JOIN {{ ref('stg_users') }} AS users -- Joining the user table
+        ON costs.user_id = users.user_id
+    GROUP BY costs.user_id, users.name -- Grouping by both user_id and user_name
 )
 
 SELECT
     user_id,
+    user_name, -- Including user name in the final result
     total_spend
 FROM user_spends
 ORDER BY user_id
