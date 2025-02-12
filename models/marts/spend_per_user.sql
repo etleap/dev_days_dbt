95982798a298d0a571b5a5fb8071e53538a0249a
Shilpa Johncy <110816050+SJohncy@users.noreply.github.com>
2025-02-12 13:14:16 +0000

    Update spend_per_user.sql

--- models/marts/spend_per_user.sql
+++ models/marts/spend_per_user.sql
@@ -1,5 +1,5 @@
 SELECT p.user_id, SUM(i.price) AS spend
-  FROM {{source('DBTDEMO', 'PURCHASE')}} p
-    INNER JOIN {{source('DBTDEMO', 'LINE_ITEM')}} li ON li.purchase_id = p.id
-    INNER JOIN {{source('DBTDEMO', 'ITEM')}} i ON i.item_id = li.item_id
+  FROM {{source('DBTDEMO', 'PURCHASE_SJ')}} p
+    INNER JOIN {{source('DBTDEMO', 'LINE_ITEM_SJ')}} li ON li.purchase_id = p.id
+    INNER JOIN {{source('DBTDEMO', 'ITEM_SJ')}} i ON i.item_id = li.item_id
   GROUP BY p.user_id
