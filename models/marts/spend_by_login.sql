e23f378ef1f5b59e4a0fe72c4be2b282311c58ca
Polina Baksheieva <55092422+Polina04@users.noreply.github.com>
2025-07-01 11:57:24 +0100

    Fix build

--- models/marts/spend_by_login.sql
+++ models/marts/spend_by_login.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='view') }}
 
-SELECT s.user_id, SUM(s.spend)/SUM(l.logins) AS spend_by_login 
+SELECT s.user_id, SUM(s.spend)/SUM(l.logins) AS spend_by_login_num
 FROM {{ref('spend_per_user')}} s
   INNER JOIN {{ref('logins_by_user')}} l ON l.user_id = s.user_id
 GROUP BY s.user_id
-ORDER BY spend_by_login desc
+ORDER BY spend_by_login_num desc
