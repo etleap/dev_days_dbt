c910846db91662fff6b3b57a32ccb557d9ff503b
Polina Baksheieva (polina@etleap.com) <polina@etleap.com>
2025-07-17 12:01:40 +0000

    Updated int_symbol_order_5min. Deleted int_watchlist_anomaly.

--- models/trade_anomaly/int_symbol_order_5min.sql
+++ models/trade_anomaly/int_symbol_order_5min.sql
@@ -1,21 +1,20 @@
 {{ config(
     pre_hook=[
-        "ALTER ICEBERG TABLE PUBLIC.\"ORDERS_KqRs6Yo0\" REFRESH",
-        "set MIN_DATE = dateadd(minute, -5, current_timestamp())"
+        "ALTER ICEBERG TABLE PUBLIC.\"TRADE_ANOMALY_rCnLfXHS\" REFRESH"
     ]
 ) }}
 
 select
-  account_id,
-  symbol,
-  sum(qty)                                   as shares_submitted_10m,
-  sum(filled_qty)                            as shares_filled_10m,
-  count_if(status = 'CANCELED')              as cancels_10m,
-  sum(
-    case when status = 'OPEN' 
-      then pnl_usd 
-      else 0 
-    end) as pnl_risk_usd,
-  max(updated_at)                            as last_update
-from {{ source('PUBLIC', 'ORDERS') }}
-group by account_id, symbol
+  account.zenith_account_id__c as account_id,
+  account.name as account_name,
+  anomaly.symbol__c as symbol,
+  anomaly.severity__c as anomaly_severity,
+  anomaly.reason__c as anomaly_reason,
+  anomaly.created_date as anomaly_create_date,
+  watchlist.watchlist_date,
+  watchlist.watch_reason
+from {{ source('PUBLIC', 'TRADE_ANOMALY') }} anomaly
+join {{ source('PUBLIC', 'SALESFORCE_ACCOUNT' )}} account
+  on anomaly.account__c = account.id
+join {{ source('PUBLIC', 'WATCHLIST') }} watchlist
+  on watchlist.entity_name = account.name
