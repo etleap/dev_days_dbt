b300364814495a2e4394ed209699c92326dffdd0
Luqman Liaquat (luqman@etleap.com) <luqman@etleap.com>
2025-07-22 09:49:46 +0000

    Updated spend_per_user.

--- models/marts/spend_per_user.sql
+++ models/marts/spend_per_user.sql
@@ -1,5 +1,5 @@
-SELECT p.user_id, SUM(i.price) AS spend
-  FROM {{source('DBTDEMO', 'PURCHASE')}} p
-    INNER JOIN {{source('DBTDEMO', 'LINE_ITEM')}} li ON li.purchase_id = p.id
+SELECT ps.user_id, SUM(i.price) AS spend
+  FROM {{source('DBTDEMO', 'PURCHASE')}} ps
+    INNER JOIN {{source('DBTDEMO', 'LINE_ITEM')}} li ON li.purchase_id = ps.id
     INNER JOIN {{source('DBTDEMO', 'ITEM')}} i ON i.item_id = li.item_id
-  GROUP BY p.user_id
+  GROUP BY ps.user_id
