0f501f5f175df91232f9c505ad50a42f143ab161
Archie Menzies (archie@etleap.com) <archie@etleap.com>
2025-07-25 13:53:02 +0000

    Updated fct_anomaly_context.

--- models/trade_anomaly/fct_anomaly_context.sql
+++ models/trade_anomaly/fct_anomaly_context.sql
@@ -1,6 +1,6 @@
 select
   symbol_order.account_id
-  symbol_order.symbol, 
+  symbol_order.symbol,
   symbol_order.pnl_risk_usd as open_exposure_usd,
   watchlist_anomaly.account_name,
   watchlist_anomaly.anomaly_create_date,
