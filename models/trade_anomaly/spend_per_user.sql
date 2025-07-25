2e9817559ee40bf6f9befdeee4696a14c634ca8f
Polina Baksheieva (polina@etleap.com) <polina@etleap.com>
2025-07-25 11:04:29 +0000

    Updated spend_per_user.

--- models/trade_anomaly/spend_per_user.sql
+++ models/trade_anomaly/spend_per_user.sql
@@ -7,6 +7,4 @@ select
   watchlist_anomaly.watch_reason,
 SELECT p.user_id, SUM(i.price) AS spend
   FROM {{source('DBTDEMO', 'PURCHASE')}} p
-    INNER JOIN {{source('DBTDEMO', 'LINE_ITEM')}} li ON li.purchase_id = p.id
-    INNER JOIN {{source('DBTDEMO', 'ITEM')}} i ON i.item_id = li.item_id
-  GROUP BY p.user_id
+    INNER JOIN {{source('DBTDEMO', 'LINE_ITEM')}} li ON li.purchase_id 
