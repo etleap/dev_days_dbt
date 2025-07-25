bf35d1a614ba36fce43a14716a0f0561129be036
Polina Baksheieva (polina@etleap.com) <polina@etleap.com>
2025-07-25 11:03:06 +0000

    Updated spend_per_user. Deleted fct_anomaly_context.

--- models/trade_anomaly/spend_per_user.sql
+++ models/trade_anomaly/spend_per_user.sql
@@ -1,3 +1,10 @@
+select
+  symbol_order.account_id,
+  symbol_order.symbol,
+  symbol_order.pnl_risk_usd as open_exposure_usd,
+  watchlist_anomaly.account_name,
+  watchlist_anomaly.anomaly_create_date,
+  watchlist_anomaly.watch_reason,
 SELECT p.user_id, SUM(i.price) AS spend
   FROM {{source('DBTDEMO', 'PURCHASE')}} p
     INNER JOIN {{source('DBTDEMO', 'LINE_ITEM')}} li ON li.purchase_id = p.id
