406abf3a303dfdcfa2281d0637584a9c1c22bd59
Archie Menzies (archie@etleap.com) <archie@etleap.com>
2025-07-29 15:59:06 +0000

    Updated fct_anomaly_context.

--- models/marts/fct_anomaly_context.sql
+++ models/marts/fct_anomaly_context.sql
@@ -1,4 +1,15 @@
-SELECT u.id AS user_id, COUNT(e.external_id) AS logins 
-FROM {{source('DBTDEMO', 'USER_INFO')}} u, {{source('DBTDEMO', 'WEB_EVENTS')}} e
-WHERE u.external_id = e.external_id
-GROUP BY u.id
+select
+  symbol_order.account_id,
+  symbol_order.symbol,
+  symbol_order.pnl_risk_usd as open_exposure_usd,
+  watchlist_anomaly.account_name,
+  watchlist_anomaly.anomaly_create_date,
+  watchlist_anomaly.watch_reason,
+  watchlist_anomaly.watchlist_date,
+  watchlist_anomaly.anomaly_reason,
+  watchlist_anomaly.anomaly_severity
+from {{ ref('int_symbol_order_5min') }} symbol_order
+left join {{ ref('int_watchlist_anomaly') }} watchlist_anomaly
+  on symbol_order.account_id = watchlist_anomaly.account_id
+  and symbol_order.symbol = watchlist_anomaly.symbol
+  and watchlist_anomaly.anomaly_create_date > dateadd(minute, -5, current_timestamp())
