8482cb73c60424b3c496e553331a8be1befba7c4
Polina Baksheieva (polina@etleap.com) <polina@etleap.com>
2025-07-31 10:13:50 +0000

    Updated spend_per_user.

--- models/spend_per_user.sql
+++ models/spend_per_user.sql
@@ -2,4 +2,3 @@ SELECT p.user_id, SUM(i.price) AS spend
   FROM {{source('DBTDEMO', 'PURCHASE')}} p
     INNER JOIN {{source('DBTDEMO', 'LINE_ITEM')}} li ON li.purchase_id = p.id
     INNER JOIN {{source('DBTDEMO', 'ITEM')}} i ON i.item_id = li.item_id
-  GROUP BY p.user_id
