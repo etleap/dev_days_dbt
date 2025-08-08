cc925caaf740ccef06cc888355d6a5ca1b2a23cd
Polina Baksheieva (polina@etleap.com) <polina@etleap.com>
2025-08-08 17:10:24 +0000

    Updated fct_anomaly_context.

--- models/trade_anomaly/fct_anomaly_context.sql
+++ models/trade_anomaly/fct_anomaly_context.sql
@@ -1,6 +1,6 @@
 select
   symbol_order.symbol,
-  symbol_order.pnl_risk_usd as open_exposure_usd,
+open_exposure_usd,
   watchlist_anomaly.account_name,
   watchlist_anomaly.anomaly_create_date,
   watchlist_anomaly.watch_reason,
