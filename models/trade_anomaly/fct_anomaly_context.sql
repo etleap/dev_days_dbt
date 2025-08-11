d8a4271f44b2bd2ea145f7affe87196777ebd8a4
Polina Baksheieva (polina@etleap.com) <polina@etleap.com>
2025-08-11 15:59:37 +0000

    Updated fct_anomaly_context.

--- models/trade_anomaly/fct_anomaly_context.sql
+++ models/trade_anomaly/fct_anomaly_context.sql
@@ -4,5 +4,3 @@ select
 
 from {{ ref('int_symbol_order_5min') }} symbol_order
 left join {{ ref('int_watchlist_anomaly') }} watchlist_anomaly
-  on symbol_order.account_id = watchlist_anomaly.account_id
-  and symbol_order.symbol = watchlist_anomaly.symbol
