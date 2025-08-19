3eb3a0926da78e9f05f688004af4db7711e768b0
Polina Baksheieva (polina@etleap.com) <polina@etleap.com>
2025-08-19 11:15:08 +0000

    Updated int_watchlist_anomaly.

--- models/trade_anomaly/int_watchlist_anomaly.sql
+++ models/trade_anomaly/int_watchlist_anomaly.sql
@@ -1,3 +1,8 @@
+{{ config(
+    pre_hook=[
+        "ALTER ICEBERG TABLE PUBLIC.\"TRADE_ANOMALY_rCnLfXHS\" REFRESH"
+    ]
+) }}
 select
   account.zenith_account_id__c as account_id,
   account.name as account_name,
@@ -8,3 +13,8 @@ select
   watchlist.watchlist_date,
   watchlist.watch_reason
 from {{ source('PUBLIC', 'TRADE_ANOMALY') }} anomaly
+join {{ source('PUBLIC', 'SALESFORCE_ACCOUNT' )}} account
+  on anomaly.account__c = account.id
+join {{ source('PUBLIC', 'WATCHLIST') }} watchlist
+  on watchlist.entity_name = account.name
+  
