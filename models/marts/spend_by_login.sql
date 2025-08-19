94de33265cb140db2486673ca99f11e98cef254d
Polina Baksheieva (polina@etleap.com) <polina@etleap.com>
2025-08-19 11:12:01 +0000

    Updated spend_by_login.

--- models/marts/spend_by_login.sql
+++ models/marts/spend_by_login.sql
@@ -4,4 +4,3 @@ SELECT s.user_id_1, SUM(s.spend)/SUM(l.logins) AS spend_by_login
 FROM {{ref('spend_per_user')}} s
   INNER JOIN {{ref('logins_by_user')}} l ON l.user_id_1 = s.user_id_1
 GROUP BY s.user_id_1
-ORDER BY spend_by_login desc
