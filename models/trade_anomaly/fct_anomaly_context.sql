d5568083807152704e05a420d71422f811aa0cc0
Matthew Korus (matt@etleap.com) <matt@etleap.com>
2025-08-20 12:13:26 +0000

    Updated fct_anomaly_context.

--- models/trade_anomaly/fct_anomaly_context.sql
+++ models/trade_anomaly/fct_anomaly_context.sql
@@ -1,6 +1,6 @@
 select
-  symbol_order.account_id,
-  symbol_order.symbol,
+  symbol_order.account_id as account_id,
+  symbol_order.symbol as symbol,
   symbol_order.pnl_risk_usd as open_exposure_usd,
   watchlist_anomaly.account_name,
   watchlist_anomaly.anomaly_create_date,
