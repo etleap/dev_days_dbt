bbe52de0d98e6fbce710e1bf396625906533776e
Demo User (demo@etleap.com) <demo@etleap.com>
2025-10-07 18:33:10 +0000

    Updated sales_summary.

--- models/sales_summary.sql
+++ models/sales_summary.sql
@@ -1,1 +1,1 @@
-SELECT * from {{ source('PUBLIC', 'SALESFORCE_ACCOUNT') }}
+SELECT id AS salesforce_id, zenith_account_id__c as account_id, name, account_number, annual_revenue FROM {{ source('PUBLIC', 'SALESFORCE_ACCOUNT') }}
