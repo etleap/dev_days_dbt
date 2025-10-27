6bd386c566e010acd6bd14e9638e8480b30add78
Demo User (demo@etleap.com) <demo@etleap.com>
2025-10-27 11:30:14 +0000

    Updated customer_summary.

--- models/customer_summary.sql
+++ models/customer_summary.sql
@@ -2,4 +2,4 @@ SELECT
 	id AS salesforce_id, 
     zenith_account_id__c AS account_id, 
     name, account_number, annual_revenue 
-FROM {{ source('PUBLIC', 'SALESFORCE_ACCOUNT') }}
+FROM {{ source('PUBLIC', 'SALESFORCE_ACCOUNT') }} 
