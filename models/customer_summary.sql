179b9350529b345d4213022eef2978ce4c70f712
Demo User (demo@etleap.com) <demo@etleap.com>
2026-01-09 18:48:20 +0000

    Updated customer_summary.

--- models/customer_summary.sql
+++ models/customer_summary.sql
@@ -3,7 +3,3 @@ SELECT
     zenith_account_id__c AS account_id, 
     name, account_number, annual_revenue 
 FROM {{ source('PUBLIC', 'SALESFORCE_ACCOUNT') }}
-{% if condition %}
-    {{ log("hi there",true) }}
-{% endif %}
-{% endmacro %} 
