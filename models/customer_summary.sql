0ff03171997759e5051b6e482907ac4347c86b19
Polina Baksheieva (polina@etleap.com) <polina@etleap.com>
2026-01-12 16:20:23 +0000

    Updated customer_summary.

--- models/customer_summary.sql
+++ models/customer_summary.sql
@@ -3,3 +3,22 @@ SELECT
     zenith_account_id__c AS account_id, 
     name, account_number, annual_revenue 
 FROM {{ source('PUBLIC', 'SALESFORCE_ACCOUNT') }}
+
+SELECT 
+	id AS salesforce_id, 
+    zenith_account_id__c AS account_id, 
+    name, account_number, annual_revenue 
+FROM {{ source('PUBLIC', 'SALESFORCE_ACCOUNT') }}
+
+-- jinja comment:
+{#
+	{{ source('PUBLIC', 'SALESFORCE_ACCOUNT') }}
+    select * from
+#}
+
+-- sql comment:
+/*
+    {{ config(...) }}
+    {{ source('PUBLIC', 'SALESFORCE_ACCOUNT') }}
+    select * from
+*/
