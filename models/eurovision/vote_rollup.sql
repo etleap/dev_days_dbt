ec78507eff92fb290ed0f5fabf4494e2c173f847
Caius Brindescu <caius@etleap.com>
2026-05-22 16:43:05 -0700

    Updated model with new names

--- models/eurovision/vote_rollup.sql
+++ models/eurovision/vote_rollup.sql
@@ -14,7 +14,7 @@ with new_votes as (
     countryCode,
     count(voteId) as new_vote_count,
     max(vote_time) as new_last_vote_time
-  from {{ source('bronze', 'votes_snowflake') }}
+  from {{ source('BRONZE', 'VOTES_SNOWFLAKE') }}
   where vote_time > (select max(last_vote_time) from {{ this }})
   group by countryCode
 )
@@ -32,7 +32,7 @@ select
   countryCode,
   count(voteId) as vote_count,
   max(vote_time) as "last_vote_time"
-from {{ source('bronze', 'votes_snowflake') }}
+from {{ source('BRONZE', 'VOTES_SNOWFLAKE') }}
 group by countryCode
 
 {% endif %}
